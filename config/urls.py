3654ad870605b296d2d63f102f7d6bc6ac042cce
lUkAC1234 <113376248+lUkAC1234@users.noreply.github.com>
2024-08-30 00:35:23 +0500

    Projects

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from web import views
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('web.urls')),
 ]
 
